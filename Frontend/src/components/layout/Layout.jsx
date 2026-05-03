5546d41122bc74aea9cd07735d3adb6ac19beccc
waisshaikh <waisshaikh394@gmail.com>
2026-05-04 03:13:37 +0530

    fix: refactor Layout and Topbar components for improved structure and styling

--- Frontend/src/components/layout/Layout.jsx
+++ Frontend/src/components/layout/Layout.jsx
@@ -1,22 +1,20 @@
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 import { Outlet } from "react-router-dom";
-import { TabsProvider } from "../../Context/TabsContext";
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
-    <TabsProvider>
-    <div className="flex h-screen">
+    <div className="flex h-screen overflow-hidden">
       <Sidebar />
 
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
         <Topbar />
-        <div className="flex-1 overflow-auto h-full overflow-y-auto custom-scrollbar ">
-          <Outlet/>
-          </div>
+
+        <main className="flex-1 min-w-0 overflow-y-auto custom-scrollbar">
+          <Outlet />
+        </main>
       </div>
     </div>
-    </TabsProvider>
   );
 };
 
--- Frontend/src/components/layout/Topbar.jsx
+++ Frontend/src/components/layout/Topbar.jsx
@@ -1,19 +1,23 @@
 import { Search } from "lucide-react";
 
 import { useAuth } from "../../features/auth/AuthContext";
-import Tabs from "../../Project/Tabs";
+
 
 const Topbar = () => {
   const { user, logout } = useAuth();
 
-  return (
-    <div className="bg-[#0F172A] border-b border-gray-800 p-3 flex justify-end gap-4">
-      <div className="flex-1 min-w-0 overflow-x-auto">
-        <Tabs />
-      </div>
+ return (
+  <div className="bg-[#0F172A] border-b border-gray-800 p-3 flex justify-between gap-4">
+    <h1 className="text-lg font-semibold text-white flex items-center">
+      Dashboard
+    </h1>
 
+    <div className="flex gap-4 items-center">
       <div className="relative max-w-64">
-        <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" size={18} />
+        <Search
+          className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"
+          size={18}
+        />
         <input
           type="text"
           placeholder="Search anything..."
@@ -25,7 +29,12 @@ const Topbar = () => {
         <span className="max-w-36 truncate text-sm text-gray-300">
           {user?.username}
         </span>
-        <img src="https://i.pravatar.cc/40" className="rounded-full w-8 h-8" />
+
+        <img
+          src="https://i.pravatar.cc/40"
+          className="rounded-full w-8 h-8"
+        />
+
         <button
           onClick={logout}
           className="rounded-lg bg-[#1E293B] px-3 py-2 text-xs text-gray-200 hover:bg-slate-700"
@@ -34,7 +43,8 @@ const Topbar = () => {
         </button>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default Topbar;
