490abd5c48b36e5308fd94299e0b7fd9d8efb83c
waisshaikh <waisshaikh394@gmail.com>
2026-05-04 11:54:19 +0530

    fix: update CORS configuration to remove trailing slash from allowed origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -29,7 +29,7 @@ app.use(
           "http://127.0.0.1:5173",
           "http://localhost:5174",
           "http://127.0.0.1:5174",
-          "https://tablioone.com/"
+          "https://tablioone.com"
         ];
 
         if (localOrigins.includes(origin)) {
