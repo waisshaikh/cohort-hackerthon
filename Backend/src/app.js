c38e8f68ac5237932029ac4d9aa81208196d6aff
waisshaikh <waisshaikh394@gmail.com>
2026-05-04 11:47:31 +0530

    feat: add additional allowed origin for CORS configuration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -29,6 +29,7 @@ app.use(
           "http://127.0.0.1:5173",
           "http://localhost:5174",
           "http://127.0.0.1:5174",
+          "https://tablioone.com/"
         ];
 
         if (localOrigins.includes(origin)) {
